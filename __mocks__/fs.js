57d30a560d0f13bc273a9bd1c0396ce366da7560
Markis Taylor <m@rkis.cc>
2018-01-29 22:51:12 -0800

    refactor tests and raise coverage

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,14 +1,42 @@
-const fs = require.requireActual('fs');
+const path = require('path');
 
-const mockCB = function (event, cb) { if (cb) cb(); };
-fs.watch = jest.fn().mockReturnValue({
-  on: mockCB,
-  once: mockCB,
-  close: jest.fn(),
-});
-fs.write = jest.fn();
-fs.writeSync = jest.fn();
-fs.writeFile = jest.fn();
-fs.writeFileSync = jest.fn();
+const realFs = require.requireActual('fs');
+const fs = jest.genMockFromModule('fs');
+
+let regexes = [];
+let contents = [];
+function __addMockFile(regex, content) {
+  regexes.push(regex);
+  contents.push(content);
+}
+function __resetMockFiles() {
+  regexes = [];
+  contents = [];
+}
+function readFileSync(path, options) {
+  for (let i = 0; i < regexes.length; i++) {
+    if (regexes[i].test(path)) {
+      return contents[i];
+    }
+  }
+  return realFs.readFileSync(path, options);
+}
+
+function mockCB(type, cb) {
+  cb();
+}
+
+function watch() {
+  return {
+    on: mockCB,
+    once: mockCB,
+    close: jest.fn(),
+  };
+}
+
+fs.__addMockFile = __addMockFile;
+fs.__resetMockFiles = __resetMockFiles;
+fs.readFileSync = readFileSync;
+fs.watch = watch;
 
 module.exports = fs;
