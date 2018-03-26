74ba1af88a9f7545df9e873a9d38f7ab95ccff2e
Markis Taylor <m@rkis.cc>
2018-03-26 13:43:14 -0700

    Remove unneeded line

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,6 @@ export default class JestRatchet {
 }
 
 function onRunComplete(globalConfig: Config, options: RatchetOptions) {
-  options = options;
   try {
     const coverageDirectory = findCoveragePath(globalConfig);
     const coverageSummaryPath = findCoverageSummaryPath(coverageDirectory);
