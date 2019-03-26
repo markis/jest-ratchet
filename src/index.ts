642e9cdf9b281983b35254bf662d19d22648cb0f
Markis Taylor <m@rkis.cc>
2019-03-26 11:23:16 -0400

    100% coverage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { closeSync, existsSync, mkdirSync, openSync, readFileSync, watch } from 'fs';
 
-import { getLastError } from './errors';
+import { getLastError, TimeoutError, tryOrReject } from './errors';
 import {
   Config,
   IstanbulCoverage,
@@ -16,52 +16,63 @@ import { noop } from './noop';
 import { ratchetCoverage } from './ratchet';
 
 export default class JestRatchet {
-  public getLastError: () => void = noop;
+  public getLastError: () => Error | void = noop;
   public onRunComplete: () => void = noop;
+  public runResult = Promise.resolve();
 
   constructor(
     globalConfig: Config,
     options: RatchetOptions = {},
   ) {
     if (!process.env.DISABLE_JEST_RATCHET) {
-      onRunComplete(globalConfig, options);
       this.getLastError = getLastError.bind(this, globalConfig);
+      this.runResult = onRunComplete(globalConfig, options);
+      this.runResult.catch(e => {
+        this.getLastError = () => { throw e; };
+      });
     }
   }
 }
 
-const onRunComplete = (globalConfig: Config, options: RatchetOptions) => {
-  try {
-    const coverageDirectory = findCoveragePath(globalConfig);
-    const coverageSummaryPath = findCoverageSummaryPath(coverageDirectory);
-    const jestConfigPath = findJestConfigPath(process.cwd(), process.argv);
+const onRunComplete = async (
+  globalConfig: Config,
+  options: RatchetOptions,
+): Promise<void> => new Promise(
+  (resolve, reject) => {
+    tryOrReject(reject, () => {
+      const coverageDirectory = findCoveragePath(globalConfig);
+      const coverageSummaryPath = findCoverageSummaryPath(coverageDirectory);
+      const jestConfigPath = findJestConfigPath(process.cwd(), process.argv);
 
-    if (!existsSync(coverageDirectory)) {
-      mkdirSync(coverageDirectory);
-    }
-    if (!existsSync(coverageSummaryPath)) {
-      closeSync(openSync(coverageSummaryPath, 'w'));
-    }
-
-    const watcher = watch(coverageDirectory);
-    const closeWatcher = () => watcher.close();
-    const timeoutTimer = options.timeout && setTimeout(closeWatcher, options.timeout);
-
-    watcher.once('change', () => {
-      closeWatcher();
-      if (timeoutTimer) {
-        clearTimeout(timeoutTimer);
+      if (!existsSync(coverageDirectory)) {
+        mkdirSync(coverageDirectory);
+      }
+      if (!existsSync(coverageSummaryPath)) {
+        closeSync(openSync(coverageSummaryPath, 'w'));
       }
 
-      const coverageRaw = readFileSync(coverageSummaryPath, 'utf-8');
-      const summary: IstanbulCoverage = JSON.parse(coverageRaw);
-      const threshold = globalConfig.coverageThreshold!;
-      const ratchetResult = ratchetCoverage(threshold, summary, options);
+      const watcher = watch(coverageDirectory);
+      const timeout = options.timeout;
 
-      updateFile(jestConfigPath, ratchetResult);
+      const timeoutTimer = timeout && setTimeout(() => {
+        watcher.close();
+        reject(new TimeoutError(coverageDirectory, timeout));
+      }, timeout);
+
+      watcher.once('change', () => tryOrReject(reject, () => {
+        watcher.close();
+        if (timeoutTimer) {
+          clearTimeout(timeoutTimer);
+        }
+
+        const coverageRaw = readFileSync(coverageSummaryPath, 'utf-8');
+        const summary: IstanbulCoverage = JSON.parse(coverageRaw);
+        const threshold = globalConfig.coverageThreshold!;
+        const ratchetResult = ratchetCoverage(threshold, summary, options);
+
+        updateFile(jestConfigPath, ratchetResult);
+        resolve();
+      }));
     });
-  } catch (e) {
-    // tslint:disable-next-line
-    console.error(e);
-  }
-};
+  },
+);
