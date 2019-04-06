3ffe105c26e46a58a7c288bf2e63a91e89c16cd5
Markis Taylor <m@rkis.cc>
2019-04-06 19:30:53 -0400

    100% coverage (#59)

--- src/errors.ts
+++ src/errors.ts
@@ -1,22 +1,41 @@
+/* tslint:disable:max-classes-per-file */
+
 import { Config } from './interfaces';
 
 export const NAME = 'jest-ratchet';
 
 export const getLastError = (config: Config) => {
-  const errors: string[] = [];
   const { collectCoverage, coverageReporters} = config;
-  if ((coverageReporters || []).indexOf('json-summary') === -1) {
-    errors.push(
-      `'json-summary' needs to be listed as a coverageReporter in order for ${ NAME } to work appropriately.`,
-    );
-  }
   if (!collectCoverage) {
-    errors.push(
-      `'collectCoverage' option needs to be enabled in order for ${ NAME } to work appropriately.`,
-    );
+    throw new CollectCoverageError();
   }
-
-  if (errors.length > 0) {
-    throw new Error(errors.join('\n'));
+  if ((coverageReporters || []).indexOf('json-summary') === -1) {
+    throw new JsonSummaryError();
   }
 };
+
+export const tryOrReject = async (reject: (reason?: any) => void, cb: () => void) => {
+  try {
+    cb();
+  } catch (e) {
+    reject(e);
+  }
+};
+
+export class JsonSummaryError extends Error {
+  constructor() {
+    super(`'json-summary' needs to be listed as a coverageReporter in order for ${ NAME } to work appropriately.`);
+  }
+}
+
+export class CollectCoverageError extends Error {
+  constructor() {
+    super(`'collectCoverage' option needs to be enabled in order for ${ NAME } to work appropriately.`);
+  }
+}
+
+export class TimeoutError extends Error {
+  constructor(public coverageDirectory?: string, public timeout?: number) {
+    super('Jest-Ratchet timed-out waiting for the Coverage Summary');
+  }
+}
